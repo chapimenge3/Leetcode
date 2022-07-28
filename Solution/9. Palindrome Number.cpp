c12c9a44784b33c441c696d1679266663f6c4802
chapi <chapimenge3@gmail.com>
2022-07-28 12:00:55 +0300

    Palindrome Number solution py and cpp

---
+++ Solution/9. Palindrome Number.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        long long num = x, revnum = 0, i;
+        while(x > 0){
+            i = x % 10;
+            revnum *= 10;
+            revnum += i;
+            x /= 10;
+        }
+        return num == revnum;
+    }
+};
