6f066af2f8fabf6ba0571c86cb8301e670b95d06
Chapi Menge <chapimenge3@gmail.com>
2022-11-15 21:02:04 +0300

    2 problem solved

---
+++ Solution/minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minSteps(string s, string t)
+{
+    map<char, int> sm, tm;
+
+    for (auto i : s)
+    {
+        sm[i]++;
+    }
+    for (auto i : t)
+    {
+        tm[i]++;
+    }
+    int num = 0;
+    std::map<char, int>::iterator it;
+    for (auto i : sm)
+    {
+        it = tm.find(i.first);
+        if (it != tm.end())
+        {
+            if (i.second > it->second)
+            {
+                num += (i.second - it->second);
+            }
+        }
+        else
+        {
+            num += i.second;
+        }
+    }
+
+    for (auto i : tm)
+    {
+        it = sm.find(i.first);
+        if (it != sm.end())
+        {
+            if (i.second > it->second)
+            {
+                num += (i.second - it->second);
+            }
+        }
+        else
+        {
+            num += i.second;
+        }
+    }
+
+    return num;
+}
+
+int main()
+{
+    string s, t;
+    cin >> s >> t;
+
+    cout << minSteps(s, t);
+}
